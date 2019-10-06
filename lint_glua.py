f832570c3498d9fb6fbfe6e8eb7762c0b1d54f28
Jenkins <jenkins@172-245-14-234-host.colocrossing.com>
2019-10-06 15:39:21 -0400

    Convert to a threaded approach

--- lint_glua.py
+++ lint_glua.py
@@ -1,29 +1,51 @@
+# A little over-engineered, but it lints all lua files in or below the cwd
+
 import coloredlogs, logging
 import os
+import queue
 
 from pathlib import Path
 from subprocess import Popen, PIPE
+from threading import Thread
 
 # logging setup
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger)
 
 results = []
+
+def lint_file(workpool, thread_num):
+    print("Starting Thread #{}".format(thread_num))
+
+    while not workpool.empty():
+        filename = workpool.get()
+        print("Thread #{}: Got some work!: {}".format(thread_num, filename))
+
+        command = ["/usr/bin/luac", filename]
+
+        p = Popen(command, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+
+        stderr = stderr.decode("utf-8")
+
+        if stderr and len(stderr) > 0:
+            results.append(stderr)
+
+workpool = queue.Queue()
+
 for filename in Path(os.getcwd()).glob('**/*.lua'):
-    filename = str(filename)
+    workpool.put(str(filename))
 
-    output = None
+threads = []
+THREAD_COUNT = 5
+for t in range(THREAD_COUNT):
+    thread = Thread(target=lint_file, args=(workpool,t))
+    threads.append(thread)
 
-    command = ["/usr/bin/luac", filename]
+    thread.start()
 
-    p = Popen(command, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate()
-
-    stderr = stderr.decode("utf-8")
-
-    results.append(stderr or None)
-
-results = [r for r in results if r]
+for thread in threads:
+    thread.join()
 
 if len(results) > 0:
     logger.error("GLua syntax errors have been detected")
