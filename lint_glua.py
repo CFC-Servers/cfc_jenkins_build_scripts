14934ae203daf76f9baff336553dd4f3a5dbe012
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 03:36:56 -0400

    Add lint glua script

---
+++ lint_glua.py
@@ -0,0 +1,53 @@
+# A little over-engineered, but it lints all lua files in or below the cwd
+
+import coloredlogs, logging
+import os
+import queue
+
+from pathlib import Path
+from subprocess import Popen, PIPE
+from threading import Thread
+
+# logging setup
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger, isatty=True)
+
+results = []
+
+def lint_file(workpool, thread_num):
+    while not workpool.empty():
+        filename = workpool.get()
+
+        command = ["/var/lib/jenkins/build_scripts/glualint", filename]
+
+        p = Popen(command, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = p.communicate()
+
+        stderr = stderr.decode("utf-8")
+
+        if stderr and len(stderr) > 0:
+            results.append(stderr)
+
+workpool = queue.Queue()
+
+for filename in Path(os.getcwd()).glob('**/*.lua'):
+    workpool.put(str(filename))
+
+threads = []
+THREAD_COUNT = 5
+for t in range(THREAD_COUNT):
+    thread = Thread(target=lint_file, args=(workpool,t))
+    threads.append(thread)
+
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+if len(results) > 0:
+    logger.error("GLua Style Violations have been detected")
+    [logger.error(err) for err in results]
+    exit(1)
+
+logger.info("No GLua style violations were detected!")
+
