7dec91620b5e8b0d8f9629776f4e0566bdfaf648
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-06 23:03:30 -0400

    Hotfix

--- check_moonscript_syntax.py
+++ check_moonscript_syntax.py
@@ -25,7 +25,7 @@ def lint_file(workpool, thread_num):
 
         stderr = stderr.decode("utf-8")
 
-        if stderr and stderr[:4] != "Built":
+        if stderr and stderr[:4].replace("\n", "") != "Built":
             results.append("\n" + stderr)
 
 workpool = queue.Queue()
